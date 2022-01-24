8d73966ba17efb3012e62fb9b09651c82e23f020
Damerian Dark <newbmailz@gmail.com>
2022-01-24 10:15:39 -0600

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
