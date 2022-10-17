0cbc8f941e8caddb6467cd31e91c0a695933d307
Damerian Dark <newbmailz@gmail.com>
2022-10-17 10:36:26 -0500

    switching to mysql

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,18 @@
+// module.exports = {
+//     Host: "ec2-52-201-72-91.compute-1.amazonaws.com",
+//     USER: "hxtnvgohlmlvwz",
+//     PASSWORD: "7c1e09029f93a5433087baaccd84716c321848b9f143c8ae1b57e5f92133ffde",
+//     DB: "d3uedqiblqftld",
+//     dialect: 'postgres',
+// }
+
 module.exports = {
-    Host: "ec2-52-201-72-91.compute-1.amazonaws.com",
-    USER: "hxtnvgohlmlvwz",
-    PASSWORD: "7c1e09029f93a5433087baaccd84716c321848b9f143c8ae1b57e5f92133ffde",
-    DB: "d3uedqiblqftld",
-    dialect: 'postgres',
+    Host: "jbb8y3dri1ywovy2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    USER: "u40d7qh9lgfu7gu9",
+    PASSWORD: "olpi4kmzg4lbcliv",
+    DB: "pkq586hal52ang4t",
+    dialect: 'mysql',
+    port: 3306,
 }
 
 
